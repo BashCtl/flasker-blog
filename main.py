4ee2fcfe4866239a69ca4368568577d93d543b75
darko <ki14.skakun@gmail.com>
2023-09-14 16:36:55 +0300

    working on forms

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = getenv("SECRET_KEY")
+
+
+# Create a Form Class
+class NameForm(FlaskForm):
+    name = StringField("What's Your Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 
 @app.route("/")
@@ -23,6 +37,17 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_internal_server_error(e):
     return render_template("500.html"), 500
+
+
+# Create Name Page
+@app.route("/name", methods=["GET", "POST"])
+def name_page():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ""
+    return render_template("name.html", name=name, form=form)
 
 
 if __name__ == "__main__":
